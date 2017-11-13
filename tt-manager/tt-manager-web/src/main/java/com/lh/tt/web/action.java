033663abb9b63f86cc384b1881e0c8dfe099347c
lhlh1994 <1328553657@qq.com>
2017-11-13 10:27:14 +0800

    Initial commit

---
+++ tt-manager/tt-manager-web/src/main/java/com/lh/tt/web/action.java
@@ -0,0 +1,12 @@
+package com.lh.tt.web;
+
+public class action {
+
+    public String index(){
+        String name="asdasdasdsa";
+        System.out.println(1123123);
+        return null;
+    }
+
+
+}
---
+++ tt-manager/tt-manager-web/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<img src="images/timg.png">
+asdasdasdasdasdSasdasdasdasd
+</body>
+</html>
